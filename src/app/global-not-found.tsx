1eecb9bbe7300a69b3acb4274934ff9dd895d528
Alvin1052 <alvin10821@gmail.com>
2026-05-03 15:30:11 +0700

    edit 404 page

--- src/app/global-not-found.tsx
+++ src/app/global-not-found.tsx
@@ -10,22 +10,24 @@ export const metadata: Metadata = {
 export default function GlobalNotFound() {
   return (
     <html lang='en'>
-      <body className='flex h-screen flex-col items-center justify-center gap-4'>
-        <Image
-          src={'/images/404.png'}
-          alt='404'
-          width={400}
-          height={400}
-          className='size-100 md:size-150 lg:size-200'
-        />
-
+      <body>
         <div className='flex flex-col items-center justify-center gap-4'>
-          <h1 className='text-primary-200 text-3xl font-bold md:text-4xl lg:text-6xl'>
-            <span>404</span> - Page Not Found
-          </h1>
-          <p className='text-lg font-semibold md:text-xl lg:text-2xl'>
-            This page does not exist.
-          </p>
+          <Image
+            src={'/images/404.png'}
+            alt='404'
+            width={400}
+            height={400}
+            className='size-100 md:size-150'
+          />
+
+          <div className='flex flex-col items-center justify-center gap-4'>
+            <h1 className='text-primary-200 text-3xl font-bold md:text-4xl lg:text-6xl'>
+              <span>404</span> - Page Not Found
+            </h1>
+            <p className='text-lg font-semibold md:text-xl lg:text-2xl'>
+              This page does not exist.
+            </p>
+          </div>
         </div>
       </body>
     </html>
